f827c5db38daac2fe0fd3530c295fc7bfed2a687
blockhead4 <blockhead4@naver.com>
2019-07-22 17:18:18 +0900

    add

--- Machine_Learning/CNN/ImageNet/Cat_Dog_classify/cd_01.py
+++ Machine_Learning/CNN/ImageNet/Cat_Dog_classify/cd_01.py
@@ -1,18 +1,55 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+import time
 import os
-import cv2
 
-DATADIR = r"C:\Users\Jwp\Desktop\workspace\python\Project\Machine_Learning\CNN\ImageNet\dataset\kagglecatsanddogs_3367a\PetImages"
-CATEGORIES = ["Dog", "Cat"]
 
-for category in CATEGORIES:
-    path = os.path.join(DATADIR, category)  # path to cats or dogs dir
-    for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-        # plt.imshow(img_array, cmap="gray")
-        # plt.show()
-        break
-    break
+# gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+# sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
-print(img_array)
+X = pickle.load(open("X.pickle", "rb"))
+y = pickle.load(open("y.pickle", "rb"))
+
+X = X/255.0
+
+dense_layers = [0, 1, 2]
+layer_sizes = [32, 64, 128]
+conv_layers = [1, 2, 3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            # print(NAME)
+            tensorboard = TensorBoard(log_dir=os.path.join("logs/", NAME))
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3,3), input_shape = X.shape[1:]))
+            model.add(Activation("relu"))
+            model.add(MaxPooling2D(pool_size=(2,2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3,3)))
+                model.add(Activation("relu"))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+
+            model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+            for l in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation("relu"))
+
+            model.add(Dense(1))
+            model.add(Activation("sigmoid"))
+
+            model.compile(loss="binary_crossentropy",
+                        optimizer="adam",
+                        metrics=['accuracy'])
+
+            model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1, callbacks=[tensorboard])

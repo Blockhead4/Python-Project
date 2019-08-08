805772cc8e3e34c54033da20e65961d96bfc95d5
blockhead4 <blockhead4@naver.com>
2019-08-08 22:16:29 +0900

    add

--- Machine_Learning/Cat_Hispsterize/VScode/preview_dataset/preview_data.py
+++ Machine_Learning/Cat_Hispsterize/VScode/preview_dataset/preview_data.py
@@ -7,16 +7,16 @@ dirname = 'CAT_00'
 base_path = 'C:/Users/Jwp/Desktop/workspace/python/ML_Tools/dataset/Cat_Hipsterizer/%s' % dirname
 file_lists = sorted(os.listdir(base_path))
 
-for file_list in file_lists:
-    if '.cat' not in file_list:
+for f in file_lists:
+    if '.cat' not in f:
         continue
 
     # read landmarks
-    pd_frame = pd.read_csv(os.path.join(base_path, file_list), sep=' ', header=None)
-    landmarks = (pd_frame.as_matrix()[0][1:-1]).reshape((-1, 2)).astype(np.int)
+    pd_frame = pd.read_csv(os.path.join(base_path, f), sep=' ', header=None)
+    landmarks = (pd_frame.values[0][1:-1]).reshape((-1, 2)).astype(np.int)
 
     # load image
-    img_filename, ext = os.path.splitext(file_list)
+    img_filename, ext = os.path.splitext(f)
 
     img = cv2.imread(os.path.join(base_path, img_filename))
 
